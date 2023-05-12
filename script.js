4826bbab333a4be442c5f2d293bf8c24ee006a44
Jordan Masone <jordan.masone@gfs.com>
2023-05-12 19:46:36 -0400

    refactore

--- script.js
+++ script.js
@@ -5,31 +5,30 @@ const coordinatesEl = document.getElementById("coordinates");
 let x = 0;
 let y = 0;
 let isDrawing = false;
-let context = canvasEl.getContext("2d");
+let context = canvasEl.getContext("2d", { alpha: true, desynchronized: false, colorSpace: "srgb", willReadFrequently: true });
 let color = "black";
-let lineWidth = 10;
+let lineWidth = 15;
 
 // Drawing Function
 const draw = (x1, y1, x2, y2) => {
     context.beginPath();
     context.strokeStyle = color;
     context.lineWidth = lineWidth;
+    context.lineCap = "round";
+    context.lineJoin = "round";
     context.moveTo(x1, y1);
     context.lineTo(x2, y2);
     context.stroke();
-    context.closePath();
 }
 
 // Determine Canvas Size
-const determineViewPixels = () => {
-    canvasEl.setAttribute("height", Math.ceil(window.innerHeight * 70 / 100));
-    canvasEl.setAttribute("width", Math.ceil(window.innerWidth * 70 / 100))
-}
-determineViewPixels();
+canvasEl.setAttribute("height", Math.ceil(window.innerHeight * 80 / 100));
+canvasEl.setAttribute("width", Math.ceil(window.innerWidth * 90 / 100));
 
-// Event Listeners
-window.addEventListener("resize", () => {
-    determineViewPixels();
+canvasEl.addEventListener("mousedown", ({ clientX, clientY, target }) => {
+    x = clientX - target.offsetLeft;
+    y = clientY - target.offsetTop;
+    isDrawing = true;
 });
 canvasEl.addEventListener("mousemove", ({ clientX, clientY, target }) => {
     if (isDrawing) {
@@ -40,11 +39,6 @@ canvasEl.addEventListener("mousemove", ({ clientX, clientY, target }) => {
         x = newX;
         y = newY;
     }
-});
-canvasEl.addEventListener("mousedown", ({ clientX, clientY, target }) => {
-    x = clientX - target.offsetLeft;
-    y = clientY - target.offsetTop;
-    isDrawing = true;
 });
 canvasEl.addEventListener("mouseup", ({ clientX, clientY, target }) => {
     if (isDrawing) {
