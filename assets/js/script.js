42802c306d6d428e9c35729763a1610d8ce60d83
Jordan Masone <jordan.masone@gfs.com>
2023-05-21 16:49:35 -0400

    Testing for touch

--- assets/js/script.js
+++ assets/js/script.js
@@ -155,6 +155,9 @@ downloadBtnEl.addEventListener("click", () => {
 colorEl.addEventListener("click", () => {
   colorInputEl.click();
 });
+colorEl.addEventListener("touchstart", () => {
+  colorInputEl.click()
+})
 colorInputEl.addEventListener("change", () => {
   color = colorInputEl.value;
   colorEl.style.color = colorInputEl.value;
